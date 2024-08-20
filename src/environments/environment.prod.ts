134bd937c4996bc223efa44d40fc8a46c9133393
Stéphane Devouard <sdevouard@gmail.com>
2024-08-20 12:04:35 +0200

    Added COF2 to shown datasets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   serviceURL: 'http://co-api.alwaysdata.net',
   secureURL: 'https://co-api.alwaysdata.net',
-  showCOF2: false,
+  showCOF2: true,
   forceAll: false,
 };
